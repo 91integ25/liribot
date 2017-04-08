ab29364b5046ec96581a0ad7667d046e9a79aa6c
91integ25 <91integ25@gmail.com>
2017-04-07 23:03:35 -0400

    almost done

--- liri.js
+++ liri.js
@@ -5,6 +5,7 @@ var toSearch = process.argv[3];
 var spotify = require('spotify');
 var Twitter = require('twitter');
 var rp = require('request-promise');
+var fs = require('fs');
 var client = new Twitter({
 	consumer_key: keys.consumer_key,
 	consumer_secret: keys.consumer_secret,
@@ -38,25 +39,59 @@ client.get('statuses/user_timeline',params, function(error,tweets,response){
 }
 
 
-function spotifyIt(){
-	var params ={
-		type:"track",
-		limit:2,
-		query:toSearch
+// function spotifyIt(){
+// 	var params ={
+// 		type:"track",
+// 		limit:2,
+// 		query:toSearch
 		
-	}
+// 	}
 
-spotify.search(params,function(err,data){
-	if(err){
-		console.log("error occurred: " + err)
-		return;
+// spotify.search(params,function(err,data){
+// 	if(err){
+// 		console.log("error occurred: " + err)
+// 		return;
+// 	}
+// data.tracks.items.map(function(e){
+// console.log(e.preview_url);
+// });
+// });
+// }
+function spotifyIt(){
+	var url = "https://api.spotify.com/v1/search?q=" + toSearch +"&type=track&limit=1"
+	if(toSearch === undefined){
+		 url = "https://api.spotify.com/v1/search?q=the sign&type=track&limit=4" ;
 	}
-data.tracks.items.map(function(e){
-console.log(e.preview_url);
-});
+rp(url)
+.then(function(body){
+
+	var parsedBody = JSON.parse(body);
+	if(toSearch === undefined){
+		var ace = parsedBody.tracks.items[2];
+
+		console.log("Artist name: " + ace.artists[0].name);
+		console.log("Song name: " +  ace.name);
+	    console.log("Preview url: " + ace.preview_url);
+	    console.log("album name: " + ace.album.name);
+	    return;
+	}
+	parsedBody.tracks.items.map(function(e){
+		 
+		console.log("Artist name: " + e.artists[0].name);
+		console.log("Song name: " + e.name);
+		console.log("Preview url: " + e.preview_url);
+		console.log("album name: " + e.album.name);
+	});
+})
+.catch(function(error){
+	console.log(error);
 });
 }
 function movie(){
+	
+	if(toSearch === undefined){
+		toSearch = "Mr.nobody";
+	}
 rp("http://www.omdbapi.com/?t="+ toSearch + "&y=&plot=short&r=json")
 .then(function(body){
 	var parsedJson =JSON.parse(body) 
@@ -67,6 +102,7 @@ console.log("Country: " + parsedJson.Country);
 console.log("Language: " + parsedJson.Language);
 console.log("Plot: " + parsedJson.Plot);
 console.log("Actors: " + parsedJson.Actors);
+//need to work on rotten tomatoes rating data
 console.log("Rotten Tomatoes: " + parsedJson.Ratings.value)
 
 })
@@ -74,4 +110,4 @@ console.log("Rotten Tomatoes: " + parsedJson.Ratings.value)
 	console.log(error);
 });
 }
-movie();
+
